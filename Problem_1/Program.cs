25149b9b442388c776d204e1b36fff18609f180e
Aidan <aidanabat@gmail.com>
2020-07-03 22:03:29 +0800

    Added Problem One Folder

---
+++ Problem_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int n = 3; n < 1000; n++)
+            {
+                if (n % 3 == 0 || n % 5 == 0)
+                {
+                    sum += n;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
