baa672581cb48eb7bcd9c81659e4a8a3cced60ad
Aidan <aidanabat@gmail.com>
2020-07-03 22:18:43 +0800

    Added Problem Two Folder

---
+++ Problem_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double prevprevNumber = 0;
+            double prevNumber = 1;
+            double sum = 0;
+            for(double n = prevNumber; n < 4000000; n += prevprevNumber)
+            {
+                if(n % 2 == 0)
+                {
+                    sum += n;
+                }
+                prevprevNumber = prevNumber;
+                prevNumber = n;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
